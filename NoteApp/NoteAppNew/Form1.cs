ca922f392a44c7e49dcbea3280dffa722cd63e61
Fririz <60225856+Fririz@users.noreply.github.com>
2020-07-02 10:12:10 +0000

    Add files via upload

---
+++ NoteApp/NoteAppNew/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteAppNew
+{
+    public partial class Form1 : Form
+    {
+
+        CryptSave cryptSave = new CryptSave();
+        Update UP = new Update();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //SAVE
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cryptSave.Save(textBox1.Text, cryptSave.TextPath, textBox2.Text);
+        }
+        //LOAD
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists(cryptSave.TextPath))
+            {
+                textBox1.Text = cryptSave.Decrypt(File.ReadAllText(cryptSave.TextPath), textBox2.Text);
+            }
+
+        }
+        //CLEAR
+        private void button3_Click(object sender, EventArgs e)
+        {
+            File.Delete(cryptSave.TextPath);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //Update
+        private void button4_Click(object sender, EventArgs e)
+        {
+            UP.Download();
+        }
+    }
+}
---
+++ NoteApp/NoteAppNew/Update.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace NoteAppNew
+{
+    class Update
+    {
+        public void Download()
+        {
+            string remoteUri = "https://1drv.ms/u/s!Av2jxua4bSzGiR3GQSG0MyiZRPcT?e=1U0We6";
+            string fileName = "NoteAppNew123123.exe", myStringWebResource = null;
+
+            WebClient myWebClient = new WebClient();
+
+            myStringWebResource = remoteUri + fileName;
+
+            myWebClient.DownloadFile(myStringWebResource, fileName);
+        }
+    }
+}
